49ac2f435f5c5f662d30a2d8e20872aef5f6465a
cristianoAntonio <criscaverzan8@gmail.com>
2021-11-22 16:53:38 -0300

    formatacao

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-float calculaMediaPonderada(float array[], int size){
-    float total = 0;
-    for(int i=0;i<size;i++){
-        total+=array[i];
-    }
-    return total/size; 
-}
-
 float calculaMS(float media){
     printf("MS = MEDIA PONDERADA + 10%%\nMS = %.2f * 1.1 = %.2f\n", media, media*1.1);
     return media*1.1;
@@ -16,16 +8,6 @@ float calculaMS(float media){
 float calculaMI(float media){
     printf("MI = MEDIA PONDERADA - 10%%\nMI = %.2f * 0.9 = %.2f\n", media, media*0.9);
     return media*0.9;
-}
-
-float lista2A(float array[], int size, float ms, float mi){
-    int cont = 0;
-    for(int i=0; i<size; i++){
-        if(array[i]<mi || array[i]>ms){
-            cont++;
-        }
-    }
-    return cont*100/size;
 }
 
 float lista2B(float array[], int size, float nota){
@@ -54,10 +36,26 @@ float calculaMP(float array[], int size){
 }
 
 void lista2_A(float array[], int size){
-    float mp, ms, mi;
+    float mp, ms, mi, prob;
+    printf("Lista 2 questao A)\n");
     mp = calculaMP(array, size);
     ms = calculaMS(mp);
     mi = calculaMI(mp);
+    int contMS=0, contMI=0;
+    for(int i=0;i<size;i++){
+        if(array[i]>ms){
+            contMS++;
+        }
+        else if(array[i]<mi){
+            contMI++;
+        }
+    }
+    prob = (contMI+contMS)*100/size;
+    printf("probabilidade que uma nota sorteada da amostra seja menor que MI ou maior que MS:\n");
+    printf("Quantidade de notas menores que MI: %d\n", contMI);
+    printf("Quantidade de notas maiores que MS: %d\n", contMS);
+    printf("Quantidade total de notas: %d\n", size);
+    printf("PROBABILIDADE: %.2f%%\n", prob);
 }
 
 int main(){
