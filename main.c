c6333d5a1cb8b7a19040d749a2f7aea473d1ce86
cristianoAntonio <criscaverzan8@gmail.com>
2021-11-22 16:40:45 -0300

    formatacao

--- main.c
+++ main.c
@@ -9,10 +9,12 @@ float calculaMediaPonderada(float array[], int size){
 }
 
 float calculaMS(float media){
+    printf("MS = MEDIA PONDERADA + 10%%\nMS = %.2f * 1.1 = %.2f\n", media, media*1.1);
     return media*1.1;
 }
 
 float calculaMI(float media){
+    printf("MI = MEDIA PONDERADA - 10%%\nMI = %.2f * 0.9 = %.2f\n", media, media*0.9);
     return media*0.9;
 }
 
@@ -37,9 +39,33 @@ float lista2B(float array[], int size, float nota){
     return cont*100/(size-1);
 }
 
+float calculaMP(float array[], int size){
+    float cont=0;
+    printf("MEDIA PONDERADA = (");
+    for(int i=0;i<size;i++){
+        cont+=array[i];
+        if(i==(size-1)){
+            printf("%.1f) / %d = %.2f\n", array[i], size, cont/size);
+        }else{
+            printf("%.1f + ", array[i]);
+        }
+    }
+    return cont/size;
+}
+
+void lista2_A(float array[], int size){
+    float mp, ms, mi;
+    mp = calculaMP(array, size);
+    ms = calculaMS(mp);
+    mi = calculaMI(mp);
+}
+
 int main(){
 
     float notas[] = {10.0, 6.8, 8.5, 7.0, 6.8, 7.5, 8.2, 10.0, 10.0,8.5, 7.0, 9.5, 9.5};
+    
+    lista2_A(notas, sizeof(notas)/4);
+    /*
     float mediaPonderada, ms, mi;
 
     mediaPonderada = calculaMediaPonderada(notas, sizeof(notas)/4);
@@ -49,6 +75,7 @@ int main(){
     printf("media ponderada: %.2f\nMS: %.2f\nMI: %.2f\n", mediaPonderada, ms, mi);
     printf("probA: %.2f%%\n", lista2A(notas, sizeof(notas)/4, ms, mi));
     printf("probB: %.2f%%\n", lista2B(notas, sizeof(notas)/4, 10));
+    */
 
     return 0;
 }
